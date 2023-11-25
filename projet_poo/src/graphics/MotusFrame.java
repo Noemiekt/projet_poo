2f3c0fb52c80faff159927ab2d0ba61a238d24b6
MONY ALEXANDRA <amony001@enseirb-matmeca.fr>
2023-11-25 15:03:12 +0100

    yo

--- projet_poo/src/graphics/MotusFrame.java
+++ projet_poo/src/graphics/MotusFrame.java
@@ -11,37 +11,31 @@ import java.awt.*;
 
 public class MotusFrame {
 	public static int nbLettre;
+	public static JFrame introFrame;
     public static JFrame homeFrame;
     public static JFrame gameFrame;
     public static String userInput;
     public static JTextField userInputField;
     
+    public static void main(String[] args) {
+        introFrame = MotusIntroFrame.createIntroFrame();
+        MotusFrameUtil.configureAndShowFrame(introFrame, 960, 540);
+    }
     
-    
+    public static void home() {
+    	introFrame.setVisible(false);
+        homeFrame = MotusHomeFrame.createHomeFrame();
+        MotusFrameUtil.configureAndShowFrame(homeFrame, 960, 540);
+
+    }
+
     public static void startGame(int selectedIndex) {
     	homeFrame.setVisible(false);
         gameFrame = MotusGameFrame.createGameFrame(selectedIndex);
-        MotusFrameUtil.configureAndShowFrame(gameFrame, 800, 350);
+        MotusFrameUtil.configureAndShowFrame(gameFrame, 960, 540);
 
     }
-
-    public static void main(String[] args) {
-        homeFrame = MotusHomeFrame.createHomeFrame();
-        MotusFrameUtil.configureAndShowFrame(homeFrame, 800, 350);
-        
-        
-        
-        /*GamesControls game;
-		game = new GamesControls(userInput);
-		System.out.printf("Le mot est : %s%n", game.motJoueur);
-		System.out.print("les indices de lettres bien positionnées sont : ");
-		for (int i = 0; i < game.lettresBonPos.length; i++) {
-		    System.out.print(game.lettresBonPos[i] + " ");
-		}
-		System.out.println();*/
-        
-        
-    }
+ 
 
     
     
